00f73b9ca760f833485badbc8c6f2d31728553fa
Pranik Nikose <72003857+PranikNikose@users.noreply.github.com>
2021-10-06 14:14:32 +0530

    Create pattern1.java
    ****
    ****
    ****
    ****

---
+++ Telusko/Patterns/pattern1.java
@@ -0,0 +1,17 @@
+
+public class FirstCode 
+{
+	public static void main(String a[])
+	
+	{
+		for (int j=0;j<4;j++)
+		{
+			for(int i =0;i<4;i++)
+			{
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+			
+	}
+}
