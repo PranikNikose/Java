cbbe24ed7d4a6e31705be50bf129e916207d2af5
Pranik Nikose <72003857+PranikNikose@users.noreply.github.com>
2021-10-07 15:30:38 +0530

    Create 2DArrayDemo.java

---
+++ Telusko/2DArrayDemo.java
@@ -0,0 +1,29 @@
+class FirstCode 
+{
+	public static void main(String[] args)
+	{
+		//initializing 2D array
+		int Array2D [][]= new int[3][3];
+		
+		//initialize and assign at same time 
+		int Arr2D [][] = {
+				{2,4,6},
+				{1,3,5},
+				{9,6,3}
+						 };
+		
+		//acessing the values
+		System.out.println(Arr2D[0][0]);    
+    System.out.println();
+		
+		//for jagged array as well as 2D normal array
+		for(int i=0;i<Arr2D.length;i++)
+		{
+			for (int j=0;j<Arr2D[i].length;j++)
+			{
+				System.out.print(Arr2D[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
