80a0cddb6f008ff563c143d59a7f488328a27592
Pranik Nikose <72003857+PranikNikose@users.noreply.github.com>
2021-10-07 15:40:39 +0530

    Update 2DArrayDemo.java

--- Telusko/2DArrayDemo.java
+++ Telusko/2DArrayDemo.java
@@ -7,21 +7,30 @@ class FirstCode
 		
 		//initialize and assign at same time 
 		int Arr2D [][] = {
-				{2,4,6},
-				{1,3,5},
-				{9,6,3}
+							{2,4,6,8},
+							{1,3,5},
+							{9,6,3}
 						 };
 		
-		//acessing the values
-		System.out.println(Arr2D[0][0]);    
-    System.out.println();
+		//accessing the values
+		System.out.println(Arr2D[0][0]);    System.out.println();
 		
-		//for jagged array as well as 2D normal array
+		//for jagged array as well as 2D normal array using For loop
 		for(int i=0;i<Arr2D.length;i++)
 		{
 			for (int j=0;j<Arr2D[i].length;j++)
 			{
 				System.out.print(Arr2D[i][j]+" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		//For jagged as well as normal 2D array using For-Each loop
+		for (int k[]: Arr2D)
+		{
+			for (int l:k)
+			{
+				System.out.print(l+" ");
 			}
 			System.out.println();
 		}
