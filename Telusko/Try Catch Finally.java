3f48ec23c9a6dc16576ff698d8d744cecab625df
Pranik Nikose <72003857+PranikNikose@users.noreply.github.com>
2021-10-12 13:37:01 +0530

    Create Try Catch Finally.java

---
+++ Telusko/Try Catch Finally.java
@@ -0,0 +1,25 @@
+
+class FirstCode 
+{
+	public static void main(String[] args) 
+	{
+		try 
+		{
+			int i = 2;
+			int j = 2;
+			int k = i / j;
+			System.out.println("Output is: " + k);
+		} 
+		
+		catch (Exception e) 
+		{
+			System.out.println("Error has Come");
+		}
+		
+		finally
+		{
+			System.out.println("GoodBye");
+		}
+
+	}
+}
