ecea387736a62df910c9d499ca1fed6a2f56d066
Pranik Nikose <72003857+PranikNikose@users.noreply.github.com>
2021-10-20 14:50:19 +0530

    Create Right Sided Downward Right angle Triangle pattern.java
    * * * * * 
    * * * * 
    * * * 
    * * 
    *

---
+++ Telusko/Patterns/Right Sided Downward Right angle Triangle pattern.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Test 
+{
+	public static void main(String a[]) 
+	{
+		int n=5;
+		for(int rows=1;rows<=n;rows++)
+		{
+			for (int col=rows;col<=n;col++)
+			{
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+		}
+	}
+}
