8e4824d9cd82980a1938ff1c0d6cca335bbd3a73
Pranik Nikose <72003857+PranikNikose@users.noreply.github.com>
2021-10-20 14:36:05 +0530

    Create SandGlass.java
      * * * * * * * * * 
        * * * * * * * 
          * * * * * 
            * * * 
              * 
              * 
            * * * 
          * * * * * 
        * * * * * * * 
      * * * * * * * * *

---
+++ Telusko/Patterns/SandGlass.java
@@ -0,0 +1,41 @@
+public class Test {
+	public static void main(String a[]) {
+		int n = 5;
+		// reverse Hill
+				for (int row = 1; row <= n; row++) 
+				{
+					for (int col = 1; col <= row; col++) 
+					{
+						System.out.print("  ");
+					}
+					for (int col = row; col < n; col++) /// Condition only < to make peak at top
+					{
+						System.out.print("* ");
+					}
+					for (int col = row; col <= n; col++) 
+					{
+						System.out.print("* ");
+					}
+					
+					System.out.println();
+				}
+		// Hill
+		for (int row = 1; row <= n; row++) /// Condition only < to make peak at mid
+		{
+			for (int col = row; col <= n; col++) 
+			{
+				System.out.print("  ");
+			}
+			for (int col = 1; col < row; col++) /// Condition only < to make peak top
+			{
+				System.out.print("* ");
+			}
+			for (int col = 1; col <= row; col++) 
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+	}
+}
