03a428b872da46f3a0890f7242fc871dc817a354
Pranik Nikose <72003857+PranikNikose@users.noreply.github.com>
2021-10-20 15:09:01 +0530

    Create Increasing Number Pattern.java
    1 
    2 2 
    3 3 3 
    4 4 4 4 
    5 5 5 5 5

---
+++ Telusko/Patterns/Increasing Number Pattern.java
@@ -0,0 +1,16 @@
+public class Test 
+{
+	public static void main(String a[]) 
+	{
+		int n = 5;
+		for (int rows = 1, p = 1; rows <= n; rows++, p++) 
+		{
+			for (int col = 1; col <= rows; col++) 
+			{
+				System.out.print(p + " ");
+			}
+
+			System.out.println();
+		}
+	}
+}
