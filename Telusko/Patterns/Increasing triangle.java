34e6fc42034edfec8da895b9622dc86e6da706db
Pranik Nikose <72003857+PranikNikose@users.noreply.github.com>
2021-10-20 13:41:34 +0530

    Create Increasing triangle.java
    * 
    * * 
    * * * 
    * * * * 
    * * * * *

---
+++ Telusko/Patterns/Increasing triangle.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Test 
+{
+	public static void main(String a[]) 
+	{
+		int n=5;
+		for(int rows=1;rows<=n;rows++)
+		{
+			for (int col=1;col<=rows;col++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		}
+}
